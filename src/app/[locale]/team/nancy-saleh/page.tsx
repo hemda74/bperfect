07f68d682fbb8b44ad2f26d3dd7f5ee631d44dad
hemda74 <ahmedashrafhemdan74@gmail.com>
2024-09-01 14:57:38 +0300

    .

--- src/app/[locale]/team/nancy-saleh/page.tsx
+++ src/app/[locale]/team/nancy-saleh/page.tsx
@@ -11,19 +11,19 @@ const Page = () => {
 			<TeamHeroSection
 				Description={t("TeamHeroSection.TeamMembers.TeamMember2.Body")}
 				Img={t("TeamHeroSection.TeamMembers.TeamMember2.Img")}
-				Facebook={t("TeamHeroSection.TeamMembers.TeamMember2.Links.Link1")}
-				Instagram={t("TeamHeroSection.TeamMembers.TeamMember2.Links.Link2")}
-				Youtube={t("TeamHeroSection.TeamMembers.TeamMember2.Links.Link3")}
+				// Facebook={t("TeamHeroSection.TeamMembers.TeamMember2.Links.Link1")}
+				// Instagram={t("TeamHeroSection.TeamMembers.TeamMember2.Links.Link2")}
+				// Youtube={t("TeamHeroSection.TeamMembers.TeamMember2.Links.Link3")}
 				Name={t("TeamHeroSection.TeamMembers.TeamMember2.Name")}
 			/>
-			<AboutTeam
+			{/* <AboutTeam
 				Text11={t("AboutMe.Paragraphs.TeamMembers.TeamMember2.Paragraph1.Text1")}
 				StrongText1={t("AboutMe.Paragraphs.TeamMembers.TeamMember2.Paragraph1.StrongText")}
 				Text12={t("AboutMe.Paragraphs.TeamMembers.TeamMember2.Paragraph1.Text2")}
 				Text21={t("AboutMe.Paragraphs.TeamMembers.TeamMember2.Paragraph2.Text2")}
 				StrongText2={t("AboutMe.Paragraphs.TeamMembers.TeamMember2.Paragraph2.StrongText")}
 				Text22={t("AboutMe.Paragraphs.TeamMembers.TeamMember2.Paragraph2.Text2")}
-			/>
+			/> */}
 		</>
 	);
 };
