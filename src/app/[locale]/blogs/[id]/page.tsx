2a01527a90bf67a565b6d62de92be7fc87ee33bf
hemda74 <ahmedashrafhemdan74@gmail.com>
2025-01-16 15:28:51 +0200

    update preriteer and eslint errors

--- src/app/[locale]/blogs/[id]/page.tsx
+++ src/app/[locale]/blogs/[id]/page.tsx
@@ -1,32 +1,27 @@
 import { Metadata } from "next";
-import React from "react";
-import { Blog } from "../../../../../BlogType";
-import CraftProjects from "@/Library/_Pages/Details/CraftProjects/CraftProjects";
+
 export const metadata: Metadata = {
 	title: "Blogs - Bperfect",
 	description:
 		"Stay updated with the latest insights, trends, and tips in beauty and dermatology from Bperfect.",
 };
+
 interface PageProps {
 	params: {
 		id: string;
 	};
 }
 
-const Page = async ({ params }: PageProps) => {
-	// const { id } = params;
-
+const Page = async ({ params: _params }: PageProps) => {
+	// Uncomment and modify this when needed
+	// const { id } = _params;
 	// const res = await fetch(
-	// 	`https://blog-admin-brown.vercel.app/api/32ed4982-2c04-4e37-8b73-75384a47ed5f/Blog/${id}`,
+	//   `https://blog-admin-brown.vercel.app/api/32ed4982-2c04-4e37-8b73-75384a47ed5f/Blog/${id}`
 	// );
-
 	// if (!res.ok) {
-	// 	// Handle error (e.g., 404)
-	// 	return <p>Error loading blog.</p>;
+	//   return <p>Error loading blog.</p>;
 	// }
-
 	// const blog: Blog = await res.json();
-
 	// return <CraftProjects blog={blog} />;
 };
 
