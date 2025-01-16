a8b4ffb3868900e2bce907e2f9589e543673ce1e
hemda74 <ahmedashrafhemdan74@gmail.com>
2025-01-16 16:15:41 +0200

    fix the error

--- next.config.js
+++ next.config.js
@@ -4,30 +4,27 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-	reactStrictMode: true, // Enable React strict mode for identifying potential problems
-	poweredByHeader: false, // Remove "X-Powered-By" header for improved security
+	reactStrictMode: true, // Enable React strict mode
+	poweredByHeader: false, // Remove "X-Powered-By" header for security
 	trailingSlash: false, // Disable trailing slashes in URLs
 	compiler: {
 		styledComponents: true, // Enable support for styled-components
 	},
-	output: "standalone", // Optimize for deployment as a standalone application
+	output: "standalone", // Optimize for deployment as a standalone app
 	images: {
-		domains: ["res.cloudinary.com"], // Allow images from specified domains
-		formats: ["image/avif", "image/webp"], // Enable modern image formats for performance
-		deviceSizes: [640, 768, 1024, 1280, 1600], // Specify responsive image sizes
+		domains: ["res.cloudinary.com"], // Allow images from specific domains
+		formats: ["image/avif", "image/webp"], // Enable modern image formats
+		deviceSizes: [640, 768, 1024, 1280, 1600], // Define responsive image sizes
 		minimumCacheTTL: 60, // Cache images for 60 seconds
 	},
-	experimental: {
-		appDir: true, // Enable experimental `app` directory (if applicable)
-	},
 	eslint: {
-		ignoreDuringBuilds: true, // Disable ESLint during build to prevent blocking
+		ignoreDuringBuilds: true, // Ignore ESLint during builds
 	},
 	typescript: {
-		ignoreBuildErrors: true, // Disable TypeScript errors during builds (if needed)
+		ignoreBuildErrors: true, // Ignore TypeScript errors during builds
 	},
 	webpack: (config, { isServer }) => {
-		// Custom Webpack configuration
+		// Custom Webpack configurations
 		if (!isServer) {
 			config.resolve.fallback = {
 				fs: false, // Disable Node.js `fs` module on the client-side
--- src/app/[locale]/blogs/page.tsx
+++ src/app/[locale]/blogs/page.tsx
@@ -1,15 +1,18 @@
-import { Metadata } from "next";
 import React from "react";
+import { Metadata } from "next";
 import { Locale } from "@/Library/Globals";
 import BlogHeroSection from "@/Library/_Pages/Blog/BlogHeroSection/BlogHeroSection";
 import TeamArticles from "@/Library/_Pages/Team/TeamArticles/TeamArticles";
+
 export const metadata: Metadata = {
 	title: "Blogs - Bperfect",
 	description:
 		"Stay updated with the latest insights, trends, and tips in beauty and dermatology from Bperfect.",
 };
+
 const Page = ({ params }: { params: Locale }) => {
 	const { locale } = params;
+
 	return (
 		<>
 			<BlogHeroSection />
